51e7cfc5e25c9ed5fa6d885e219fef3b02856e4f
Miguel García Reimers <mgarciareimers@gmail.com>
2020-09-06 17:47:53 +0200

    Pageview created + Dots added + NotifierProvided logic implemented.

--- backgrounds_custom_painter/lib/src/pages/slideshow_page.dart
+++ backgrounds_custom_painter/lib/src/pages/slideshow_page.dart
@@ -1,13 +1,118 @@
+import 'package:backgrounds_design/src/models/slider_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SvgPicture.asset('assets/svg/slide_1.svg'),
+    return ChangeNotifierProvider(
+      create: (BuildContext context) {
+        return new SliderModel();
+      },
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(child: _Slides()),
+              _Dots(),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+class _Slides extends StatefulWidget {
+  @override
+  __SlidesState createState() => __SlidesState();
+}
+
+class __SlidesState extends State<_Slides> {
+  final PageController pageViewController = new PageController();
+
+  @override
+  void initState() {
+    this.pageViewController.addListener(() {
+      Provider.of<SliderModel>(this.context, listen: false).currentPage = this.pageViewController.page;
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    this.pageViewController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        controller: this.pageViewController,
+        physics: BouncingScrollPhysics(),
+        children: [
+          _Slide(svg: 'assets/svg/slide_1.svg'),
+          _Slide(svg: 'assets/svg/slide_2.svg'),
+          _Slide(svg: 'assets/svg/slide_3.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  const _Slide({ Key key, this.svg }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(80),
+      child: SvgPicture.asset(this.svg),
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(index: 0),
+          _Dot(index: 1),
+          _Dot(index: 2),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  final int index;
+
+  const _Dot({Key key, @required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      width: 10,
+      height: 10,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Provider.of<SliderModel>(context).currentPage == this.index ? Colors.black : Colors.grey,
+      ),
+    );
+  }
+}
+
+
